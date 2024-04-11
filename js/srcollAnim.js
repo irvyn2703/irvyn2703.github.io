6af6bc0d00d9bb1dc2afd735beaeb0908c36d26a
irvyn2703 <tosolatoso17@gmail.com>
2024-04-11 12:07:42 -0600

    diseño completo

--- js/srcollAnim.js
+++ js/srcollAnim.js
@@ -5,14 +5,25 @@ const habilidades = document.getElementsByClassName('skills');
 const educacion = document.getElementsByClassName('educacion');
 const proyectos = document.getElementsByClassName('todos');
 const principal = document.getElementById('principal');
-console.log(principal);
+// fondo
+const montañas = document.getElementsByClassName('montaña');
+const arboles = document.getElementsByClassName('arbol');
+const sol = document.getElementsByClassName('sol');
+const aves = document.getElementsByClassName('ave');
 
+console.log(titulos)
 const observador = new IntersectionObserver(
     entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
+                for (let i = 0; i < montañas.length; i++) {
+                    montañas[i].classList.add('anim');
+                }
+                for (let i = 0; i < arboles.length; i++) {
+                    arboles[i].classList.add('anim');
+                }
                 entry.target.classList.add('anim');
-                //observador.unobserve(entry.target);
+                observador.unobserve(entry.target);
             } else {
                 entry.target.classList.remove('anim');
             }
@@ -27,8 +38,9 @@ const observadorEncabezados = new IntersectionObserver(
     entries => {
         entries.forEach(entry => {
             if (entry.isIntersecting) {
+                sol[0].classList.add('anim');
                 entry.target.classList.add('anim');
-                //observadorEncabezados.unobserve(entry.target);
+                observadorEncabezados.unobserve(entry.target);
             } else {
                 entry.target.classList.remove('anim');
             }
@@ -44,6 +56,7 @@ const observadorPrincipal = new IntersectionObserver(
         entries.forEach(entry => {
             if (entry.isIntersecting) {
                 entry.target.classList.add('anim');
+                observadorEncabezados.unobserve(entry.target);
             } else {
                 entry.target.classList.remove('anim');
             }
@@ -53,13 +66,36 @@ const observadorPrincipal = new IntersectionObserver(
     }
 )
 
+const observadorFormacion = new IntersectionObserver(
+    entries => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                for (let i = 0; i < aves.length; i++) {
+                    aves[i].classList.add('anim')
+                }
+                entry.target.classList.add('anim');
+                observadorEncabezados.unobserve(entry.target);
+            } else {
+                entry.target.classList.remove('anim');
+            }
+        });
+    },{
+        rootMargin: '0px 0px -150px 0px',
+        threshold: 1,
+    }
+)
+
 observador.observe(sobreMi[0]);
 observador.observe(datos[0]);
 observador.observe(educacion[0]);
 observador.observe(proyectos[0]);
 observadorPrincipal.observe(principal);
 for (let i = 0; i < titulos.length; i++) {
-    observadorEncabezados.observe(titulos[i]);
+    if (i === 1) {
+        observadorFormacion.observe(titulos[i]);
+    }else{
+        observadorEncabezados.observe(titulos[i]);
+    }
 }
 for (let i = 0; i < habilidades.length; i++) {
     observador.observe(habilidades[i]);
