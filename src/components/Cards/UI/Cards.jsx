3768127368374fad8abfea6d34ab844b9d386253
irvyn2703 <tosolatoso17@gmail.com>
2025-02-02 23:06:35 -0600

    style(Cards): refactor styles and update layout

--- src/components/Cards/UI/Cards.jsx
+++ src/components/Cards/UI/Cards.jsx
@@ -3,20 +3,27 @@ import styles from "../Style/Cards.module.css";
 const Cards = ({ Type }) => {
   return (
     <div
-      className={styles.container}
+      className={styles.backgroundImage}
       style={{
         backgroundImage: `url(assets/${Type?.picture})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
-        maxHeight: "70vh",
       }}
     >
-      <div className={styles.divition}>
-        <div className={styles.title}>{Type?.title}</div>
-        <div className={styles.date}>{Type?.date}</div>
+      <div className={styles.container}>
+        <div className={styles.divition}>
+          <div className={styles.title}>{Type?.title}</div>
+          <div className={styles.date}>{Type?.date || Type?.tecnologic}</div>
+        </div>
+        <div className={styles.subtitle}>{Type?.subtitle}</div>
+        <div className={styles.description}>{Type?.description}</div>
+        {Type?.url && (
+          <a href={Type.url} target="_blank" rel="noopener noreferrer">
+            <i class="fa-brands fa-github"></i>
+            <span>Ver repositorio</span>
+          </a>
+        )}
       </div>
-      <div className={styles.subtitle}>{Type?.subtitle}</div>
-      <div className={styles.description}>{Type?.description}</div>
     </div>
   );
 };
