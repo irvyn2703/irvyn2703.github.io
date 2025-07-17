226afa218509f2098f80912fb3e22013f9349007
irvyn2703 <tosolatoso17@gmail.com>
2025-07-17 16:59:32 -0600

    feat: integrate Swiper for project slides and update card styles

--- src/components/Cards/UI/Cards.jsx
+++ src/components/Cards/UI/Cards.jsx
@@ -1,29 +1,17 @@
 import styles from "../Style/Cards.module.css";
 
 const Cards = ({ item }) => {
-  const {
-    Titulo = "",
-    subtitulo = "",
-    Fecha = "",
-    Imagen = "",
-    Descripcion = "",
-  } = item;
+  const { Titulo = "", subtitulo = "", Fecha = "", Descripcion = "" } = item;
   return (
-    <div
-      className={styles.container}
-      style={{
-        backgroundImage: `url(assets/${Imagen})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        maxHeight: "70vh",
-      }}
-    >
-      <div className={styles.divition}>
-        <div className={styles.title}>{Titulo}</div>
-        <div className={styles.date}>{Fecha}</div>
+    <div className={styles.mainContainer}>
+      <div className={styles.container}>
+        <div className={styles.divition}>
+          <div className={styles.title}>{Titulo}</div>
+          <div className={styles.date}>{Fecha}</div>
+        </div>
+        <div className={styles.subtitle}>{subtitulo}</div>
+        <div className={styles.description}>{Descripcion}</div>
       </div>
-      <div className={styles.subtitle}>{subtitulo}</div>
-      <div className={styles.description}>{Descripcion}</div>
     </div>
   );
 };
--- src/sections/Proyects/UI/Proyects.jsx
+++ src/sections/Proyects/UI/Proyects.jsx
@@ -1,27 +1,47 @@
-import ButtonIcon from "../../../components/ButtonIcon/UI/ButtonIcon";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  Navigation,
+  Pagination,
+  Autoplay,
+  EffectCoverflow,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 import Cards from "../../../components/Cards/UI/Cards";
-import ProyectBusiness from "../Business/ProyectsBusiness";
 import styles from "../Style/Proyects.module.css";
 
 const Proyects = ({ proyects }) => {
-  const { proyect, handledClickLeft, handledClickRight } = ProyectBusiness({
-    proyects,
-  });
   return (
     <section className={styles.proyects} id="proyects">
       <h2>Proyectos</h2>
       <div className={styles.container}>
-        <Cards item={proyect} />
-        <div className={styles.containerButtons}>
-          <ButtonIcon
-            icon="fa-solid fa-chevron-left"
-            handlendClick={handledClickLeft}
-          />
-          <ButtonIcon
-            icon="fa-solid fa-chevron-right"
-            handlendClick={handledClickRight}
-          />
-        </div>
+        <Swiper
+          effect={"coverflow"}
+          grabCursor={true}
+          centeredSlides={true}
+          modules={[Navigation, Pagination, Autoplay, EffectCoverflow]}
+          spaceBetween={0}
+          slidesPerView={1}
+          coverflowEffect={{
+            rotate: 100,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+          }}
+          navigation
+          pagination={{ clickable: true }}
+          loop={true}
+          autoplay={{ delay: 5000, disableOnInteraction: false }}
+          className="mySwiper"
+        >
+          {proyects.map((proyect, index) => (
+            <SwiperSlide key={index}>
+              <Cards item={proyect} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </section>
   );
